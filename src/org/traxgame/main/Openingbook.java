68e188978bd7ba429a12677134e2229dbdf129ad
Martin M. S. Pedersen <traxplayer@gmail.com>
2014-02-01 08:15:25 +0100

    Working code that generates book.bin including always play positions

--- src/org/traxgame/main/Openingbook.java
+++ src/org/traxgame/main/Openingbook.java
@@ -1,6 +1,6 @@
 /* 
 
-Date: Januar 29 - 2014
+Date: Februar 1 - 2014
 version 0.1
 All source under GPL version 2 
 (GNU General Public License - http://www.gnu.org/)
@@ -8,7 +8,6 @@ contact traxplayer@gmail.com for more information about this code
 */
 
 package org.traxgame.main;
-
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -23,57 +22,78 @@ import java.util.Map;
 public class Openingbook
 {
   private String inputfile;
-  private int maxply;
-  private HashMap<bookKey,bookValue> theBook=new HashMap<bookKey,bookValue>();
+  private HashMap<bookKey,bookValue> theBook;
 
-  public Openingbook(String inputfile) { 
-	  this(inputfile,-1);
+  public Openingbook() { 
+    theBook=new HashMap<bookKey,bookValue>();
   }
 
-  public Openingbook(String inputfile, int maxply) { 
+  public void setInputfile(String inputfile) {
     this.inputfile=inputfile;
-    this.maxply=maxply;
   }
 
   public void generateBook() throws IOException {
+    this.generateBook(false);
+  }
+
+  public void generateBook(boolean winning) throws IOException {
     String s;
     Traxboard tb;
     BufferedReader reader=new BufferedReader(new FileReader(new File(inputfile)));
-    BufferedWriter writer=new BufferedWriter(new FileWriter(new File("./book.bin")));
-    while ( (s=reader.readLine()) != null) {
-      tb=new Traxboard();
-      boolean useless=false;
-      try {
-          for (String move : s.split("\\s")) { tb.makeMove(move); } 
-      } catch (IllegalMoveException e) { useless=true; }
-      if (!useless && tb.isGameOver()!=Traxboard.NOPLAYER) {
-        int gameOverValue=tb.isGameOver();
-        tb=new Traxboard();
-        for (String move : s.split("\\s")) {
-          try {
-            tb.makeMove(move);
-          }
-          catch (IllegalMoveException e) { 
-            // This should never happen
-            throw new RuntimeException(e);
-          }
-          bookKey bk=new bookKey(tb.getBorder(),tb.whoToMove());
-          bookValue bv=search(bk);
-          if (bv==null) { bv=new bookValue(); }
-          switch (gameOverValue) {
-            case Traxboard.DRAW:  bv.draw++; break;
-            case Traxboard.WHITE: bv.white++; break;
-            case Traxboard.BLACK: bv.black++; break;
-            default:
-              // This should never happen
-              throw new RuntimeException("\n"+tb+"tb.isGameOver()="+tb.isGameOver());
-          }
-          theBook.put(bk,bv);
-        }
-      }
+    if (winning==false) {
+	    while ( (s=reader.readLine()) != null) {
+		    tb=new Traxboard();
+		    boolean useless=false;
+		    try {
+			    for (String move : s.split("\\s")) { tb.makeMove(move); } 
+		    } catch (IllegalMoveException e) { useless=true; }
+		    if (!useless && tb.isGameOver()!=Traxboard.NOPLAYER) {
+			    int gameOverValue=tb.isGameOver();
+			    tb=new Traxboard();
+			    for (String move : s.split("\\s")) {
+				    try {
+					    tb.makeMove(move);
+				    }
+				    catch (IllegalMoveException e) { 
+					    // This should never happen
+					    throw new RuntimeException(e);
+				    }
+				    bookKey bk=new bookKey(tb.getBorder(),tb.whoToMove());
+				    bookValue bv=search(bk);
+				    if (bv==null) { bv=new bookValue(); }
+				    switch (gameOverValue) {
+					    case Traxboard.DRAW:  bv.draw++; break;
+					    case Traxboard.WHITE: bv.white++; break;
+					    case Traxboard.BLACK: bv.black++; break;
+					    default:
+								  // This should never happen
+								  throw new RuntimeException("\n"+tb+"tb.isGameOver()="+tb.isGameOver());
+				    }
+				    theBook.put(bk,bv);
+			    }
+		    }
+	    }
+	}	
+	else {
+	   while ( (s=reader.readLine()) != null) {
+		    tb=new Traxboard();
+		    try {
+			    for (String move : s.split("\\s")) { tb.makeMove(move); } 
+			    bookKey bk=new bookKey(tb.getBorder(),tb.whoDidLastMove());
+
+			    theBook.remove(bk);
+			    bookValue bv=new bookValue(); 
+			    bv.alwaysPlay=true;
+			    theBook.put(bk,bv);
+		    } catch (IllegalMoveException e) { ; }
+	  }
+       }
     }
+
+  private void saveBook() throws IOException {
+    BufferedWriter writer=new BufferedWriter(new FileWriter(new File("./book.bin")));
     for (Map.Entry<bookKey, bookValue> entry : theBook.entrySet()) {
-      if (entry.getValue().white+entry.getValue().black+entry.getValue().draw>2) {
+      if ((entry.getValue().alwaysPlay) || (entry.getValue().white+entry.getValue().black+entry.getValue().draw>2)) {
         writer.write(entry.getKey()+" "+entry.getValue()+"\n");
       }
     }
@@ -95,10 +115,13 @@ public class Openingbook
   }
 
   public static void main(String[] args) {
-    //Openingbook o=new Openingbook("doby3_games.txt");
-    Openingbook o=new Openingbook("t");
+    Openingbook o=new Openingbook();
     try {
+      o.setInputfile("alwaysplay.trx");
+      o.generateBook(true);
+      o.setInputfile("t");
       o.generateBook();
+      o.saveBook();
     }
     catch (Exception e) { System.err.println(e); }
   }
@@ -110,8 +133,16 @@ public class Openingbook
       if (alwaysPlay) return 1000;
       return 0;
     }
+    
+    public bookValue() { this(false); }
+
+    public bookValue(boolean alwaysPlay) { 
+	this.alwaysPlay=alwaysPlay; 
+        black=0; white=0; draw=0;
+    }
+     
     public String toString() {
-      return (alwaysPlay?"true":"false"+" "+black+" "+white+" "+draw);
+      return ((alwaysPlay?"true":"false")+" "+black+" "+white+" "+draw);
     }
   }
 
