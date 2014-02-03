d045591181b9b07f5db90db8ab30691f735eb108
Martin M. S. Pedersen <mp@superusers.dk>
2014-02-03 15:37:14 +0100

    opening book code useable for the UCT-player

--- src/org/traxgame/main/ComputerPlayerUct.java
+++ src/org/traxgame/main/ComputerPlayerUct.java
@@ -1,159 +1,202 @@
+/* 
+   Date: 5th of Februar 2014
+   version 0.1
+   All source under GPL version 2
+   (GNU General Public License - http://www.gnu.org/)
+   contact traxplayer@gmail.com for more information about this code
+*/
+
 package org.traxgame.main;
 
-/* 
- Date: 19th of October 2009
- version 0.1
- All source under GPL version 2
- (GNU General Public License - http://www.gnu.org/)
- contact traxplayer@gmail.com for more information about this code
- */
+import java.util.*;
+import java.io.*;
 
-import java.util.ArrayList;
-
-public class ComputerPlayerUct extends ComputerPlayer {
-	private int maxSimulations;
-
-	public ComputerPlayerUct() {
-		// maxSimulations = 10000;
-		maxSimulations = 100000;
+public class ComputerPlayerUct extends ComputerPlayer
+{
+    private int maxSimulations;
+    private Openingbook book=new Openingbook();
+    
+    public ComputerPlayerUct ()
+    {
+	//maxSimulations = 10000;
+	this(100000);
+    }
+    
+    public ComputerPlayerUct (int maxSimulations)
+    {
+	this.maxSimulations = maxSimulations;
+	try {
+	  book.loadBook();
 	}
-
-	public ComputerPlayerUct(int maxSimulations) {
-		this.maxSimulations = maxSimulations;
+	catch (IOException e) {
+	  System.err.println("No games/book.bin found.");
 	}
+    }
+   
+    private String simpleMove(Traxboard tb) {
+      int percent=TraxUtil.getRandom(100);
 
-	private String simpleMove(Traxboard tb) {
-		int percent = TraxUtil.getRandom(100);
+      if (tb.getNumOfTiles()==0) {
+        if (percent<50) {
+	  return new String("@0/");
+        } else {
+          return new String("@0+");
+        }
+      }
+      if (tb.getNumOfTiles()==1) {
+        if (tb.getAt(1,1)==Traxboard.NW) {
+          if (percent<15) {
+            return new String("B1\\");
+          }
+	  if (percent<90) {
+            return new String("@1/");
+          } else {
+	    return new String("B1+");
+	  }
+        }
+        if (tb.getAt(1,1)==Traxboard.NS) {
+          if (percent<5) {
+            return new String("A0+");
+          }
+          if (percent<15) {
+            return new String("A0/");
+          }
+          if (percent<25) {
+            return new String("B1/");
+          }
+          return new String("B1+");
+        }
+      }
+      if (tb.getNumOfTiles()==2) {
+        if (tb.getAt(1,1)==Traxboard.NW) {
+          if (tb.getAt(1,2)==Traxboard.NE) return new String("A2/");
+	  if (tb.getAt(2,1)==Traxboard.WS) return new String("B1/");
+        }
+      }
+      return null;
+    }
 
-		if (tb.getNumOfTiles() == 0) {
-			if (percent < 50) {
-				return new String("@0/");
-			} else {
-				return new String("@0+");
-			}
-		}
-		if (tb.getNumOfTiles() == 1) {
-			if (tb.getAt(1, 1) == Traxboard.NW) {
-				if (percent < 40) {
-					return new String("B1\\");
-				} else {
-					return new String("@1/");
-				}
-			}
-			if (tb.getAt(1, 1) == Traxboard.NS) {
-				if (percent < 10) {
-					return new String("A0+");
-				}
-				if (percent < 20) {
-					return new String("A0/");
-				}
-				if (percent < 30) {
-					return new String("B1/");
-				}
-				return new String("B1+");
-			}
-		}
-		if (tb.getNumOfTiles() == 2) {
-			if (tb.getAt(1, 1) == Traxboard.NW) {
-				if (tb.getAt(1, 2) == Traxboard.NE)
-					return new String("A2/");
-				if (tb.getAt(2, 1) == Traxboard.WS)
-					return new String("B1/");
-			}
-		}
-		return null;
+    private String openingMove(Traxboard tb) {
+      int bestScore=Integer.MIN_VALUE;
+      int factor=1;
+      String bestMove=null;
+
+      if (book==null) {
+	  return null; 
+      }
+      if (tb.whoToMove()==Traxboard.BLACK) { factor=-1; }
+
+      for (String move : tb.uniqueMoves()) {
+	 Traxboard t_copy=new Traxboard(tb);
+	 try { t_copy.makeMove(move); }
+	 catch (IllegalMoveException e) {
+	     throw new RuntimeException("This should never happen. (027)");
+	 }
+	 Openingbook.bookValue bv=book.search(t_copy);
+	 if (bv!=null) {
+	   if (bv.alwaysPlay) return move;
+	   if (bv.score()>bestScore*factor) {
+	     bestMove=move;
+	     bestScore=bv.score()*factor;
+	   }
+	 }
+      }
+      if (bestMove!=null) return bestMove;
+      return null;
+    }
+
+    public String computerMove(Traxboard tb) {
+        String simple=null;
+
+ 	simple=simpleMove(tb);
+	if (simple!=null) { return simple; }
+        simple=openingMove(tb);
+	if (simple!=null) { return simple; }
+
+	UctNode root = new UctNode(tb);
+	int maxSimulations;
+	
+	maxSimulations=this.maxSimulations/(65-tb.getNumOfTiles()/2);
+	for (int simulationCount=0; simulationCount<maxSimulations;  simulationCount++) {
+	    //System.out.println(root);
+	    playSimulation (root);
+	    if (simulationCount % 250 == 0) {
+		//System.err.print(".");
+		;
+	    }
 	}
-
-	public String computerMove(Traxboard tb) {
-		String simple = simpleMove(tb);
-		if (simple != null)
-			return simple;
-
-		UctNode root = new UctNode(tb);
-		int maxSimulations;
-
-		maxSimulations = this.maxSimulations / (65 - tb.getNumOfTiles() / 2);
-		for (int simulationCount = 0; simulationCount < maxSimulations; simulationCount++) {
-			// System.out.println(root);
-			playSimulation(root);
-			if (simulationCount % 250 == 0) {
-				// System.err.print(".");
-				;
-			}
-		}
-		// DEBUG
-		/*
-		 * ArrayList<UctNode> children=root.getChildren(); for (int i = 0; i <
-		 * children.size (); i++) {
-		 * System.err.println("Child: "+i+children.get(i)); }
-		 */
-
-		return root.getWorse().getMove();
+	// DEBUG
+	/*
+	ArrayList<UctNode> children=root.getChildren();
+	for (int i = 0; i < children.size (); i++) {
+	    System.err.println("Child: "+i+children.get(i));
 	}
-
-	private int playRandomGame(Traxboard tb) {
-		while (tb.isGameOver() == Traxboard.NOPLAYER) {
-			try {
-				tb.makeMove(TraxUtil.getRandomMove(tb));
-			} catch (IllegalMoveException e) {
-				throw new RuntimeException();
-			}
-		}
-		return tb.isGameOver();
+	*/
+	
+	return root.getWorse().getMove();
+    }
+    
+    private int playRandomGame (Traxboard tb) {
+	while (tb.isGameOver()==Traxboard.NOPLAYER) {
+	    try { tb.makeMove (TraxUtil.getRandomMove (tb)); }
+	    catch (IllegalMoveException e) { throw new RuntimeException (); }
 	}
-
-	private UctNode UCTselect(UctNode node) {
-		float value;
-		float bestUct = -1;
-		int best_index = -1;
-		ArrayList<UctNode> children = node.getChildren();
-
-		if (children == null) {
-			node.createChildren();
-			children = node.getChildren();
-		}
-		if (children.size() == 0) {
-			System.err.println(node);
-			throw new RuntimeException("Doh 2!");
-		}
-		for (int i = 0; i < children.size(); i++) {
-			value = children.get(i).UctValue();
-			if (value > bestUct) {
-				best_index = i;
-				bestUct = value;
-			}
-		}
-		// System.out.println(bestUct);
-		return children.get(best_index);
+	return tb.isGameOver();
+    }
+    
+    
+    private UctNode UCTselect (UctNode node) {
+	float value;
+	float bestUct = -1;
+	int best_index = -1;
+	ArrayList<UctNode> children=node.getChildren();
+	
+	if (children==null) {
+	    node.createChildren ();
+	    children = node.getChildren ();
 	}
+	if (children.size()==0) {
+	    System.err.println(node);
+	    throw new RuntimeException("Doh 2!");
+	}
+	for (int i = 0; i < children.size (); i++) {
+	    value = children.get (i).UctValue ();
+	    if (value > bestUct) {
+		best_index = i;
+		bestUct = value;
+	    }
+	}
+	//System.out.println(bestUct);
+	return children.get(best_index);
+    }
 
-	private int playSimulation(UctNode node) {
-		int result = -1;
-
-		if (node.getVisits() < 5) {
-			result = playRandomGame(new Traxboard(node.getPosition()));
-			node.update(result);
-			return result;
-		} else {
-			if ((node.getChildren() == null)
-					|| (node.getChildren().size() == 0)) {
-				node.createChildren();
-			}
-			UctNode next = UCTselect(node);
-			if (next == null) {
-				System.err.println(node);
-				System.err.println(next);
-				throw new RuntimeException("Doh!");
-			}
-			if (next.getPosition().isGameOver() != Traxboard.NOPLAYER) {
-				result = next.getPosition().isGameOver();
-				next.update(result);
-				return result;
-			}
-			result = playSimulation(next);
-			next.update(result);
-		}
+    private int playSimulation (UctNode node) {
+	int result=-1;
+	
+	if (node.getVisits()<5)	{
+	    result=playRandomGame(new Traxboard(node.getPosition()));
+	    node.update(result); 
+	    return result;
+	}
+	else {
+	    if ((node.getChildren()==null) || (node.getChildren().size()==0)) {
+		node.createChildren();
+	    }
+	    UctNode next=UCTselect(node);
+	    if (next==null) {
+		System.err.println(node);
+		System.err.println(next);
+		throw new RuntimeException("Doh!");
+	    }
+	    if (next.getPosition().isGameOver()!=Traxboard.NOPLAYER) {
+		result=next.getPosition().isGameOver();
+		next.update(result);
 		return result;
+	    }
+	    result=playSimulation(next);
+	    next.update(result);
 	}
-
+	return result;
+    }
+   
 }
--- src/org/traxgame/main/Openingbook.java
+++ src/org/traxgame/main/Openingbook.java
@@ -1,241 +1,275 @@
 /* 
 
-Date: Januar 25 - 2014
-version 0.1
-All source under GPL version 2 
-(GNU General Public License - http://www.gnu.org/)
-contact traxplayer@gmail.com for more information about this code
-*/
+ Date: Januar 25 - 2014
+ version 0.1
+ All source under GPL version 2 
+ (GNU General Public License - http://www.gnu.org/)
+ contact traxplayer@gmail.com for more information about this code
+ */
 
 package org.traxgame.main;
 
 import java.io.*;
 import java.util.*;
 
-public class Openingbook
-{
-  private String inputfile;
-  private HashMap<bookKey,bookValue> theBook;
+public class Openingbook {
 
-  public Openingbook() { 
-    theBook=new HashMap<bookKey,bookValue>();
-  }
+    private String inputfile;
+    private HashMap<bookKey, bookValue> theBook;
 
-  public int size() { return theBook.size(); }
+    public Openingbook() {
+       theBook = new HashMap<bookKey, bookValue>();
+    }
 
-  public void setInputfile(String inputfile) {
-    this.inputfile=inputfile;
-  }
+    public int size() {
+        return theBook.size();
+    }
 
-  public void generateBook() throws IOException {
-    this.generateBook(false);
-  }
+    public void setInputfile(String inputfile) {
+        this.inputfile = inputfile;
+    }
 
-  public void generateBook(boolean winning) throws IOException {
-    String s;
-    Traxboard tb;
-    BufferedReader reader=new BufferedReader(new FileReader(new File(inputfile)));
-    if (winning==false) {
-	    while ( (s=reader.readLine()) != null) {
-		    tb=new Traxboard();
-		    boolean useless=false;
-		    boolean resign=false;
-		    try {
-			    for (String move : s.split("\\s")) { 
-				if (move.equals("RESIGN")) break;
-				tb.makeMove(move); 
-                            } 
-		    } catch (IllegalMoveException e) { useless=true; }
-		    if (!resign && tb.isGameOver()==Traxboard.NOPLAYER) {
-                      useless=true;
+    public void generateBook() throws IOException {
+        this.generateBook(false);
+    }
+
+    public void generateBook(boolean winning) throws IOException {
+        String s;
+        Traxboard tb;
+        BufferedReader reader = new BufferedReader(new FileReader(new File(inputfile)));
+        if (winning == false) {
+            while ((s = reader.readLine()) != null) {
+                tb = new Traxboard();
+                boolean useless = false;
+                boolean resign = false;
+                try {
+                    for (String move : s.split("\\s")) {
+                        if (move.equals("RESIGN")) {
+                            break;
+                        }
+                        tb.makeMove(move);
                     }
-                    if (useless) continue;
-                    int gameOverValue;
-                    if (resign) { 
-                      gameOverValue=tb.whoDidLastMove();
-                    } else {
-		      gameOverValue=tb.isGameOver();
+                } catch (IllegalMoveException e) {
+                    useless = true;
+                }
+                if (!resign && tb.isGameOver() == Traxboard.NOPLAYER) {
+                    useless = true;
+                }
+                if (useless) {
+                    continue;
+                }
+                int gameOverValue;
+                if (resign) {
+                    gameOverValue = tb.whoDidLastMove();
+                } else {
+                    gameOverValue = tb.isGameOver();
+                }
+                tb = new Traxboard();
+                for (String move : s.split("\\s")) {
+                    try {
+                        if (move.equals("RESIGN")) {
+                            break;
+                        }
+                        tb.makeMove(move);
+                    } catch (IllegalMoveException e) {
+                        // This should never happen
+                        throw new RuntimeException(e);
                     }
-		    tb=new Traxboard();
-		    for (String move : s.split("\\s")) {
-			    try {
-				    if (move.equals("RESIGN")) break; 
-				    tb.makeMove(move);
-			    }
-			    catch (IllegalMoveException e) { 
-			    // This should never happen
-			    throw new RuntimeException(e);
-		    }
-		    bookKey bk=new bookKey(tb.getBorder(),tb.whoToMove());
-		    bookValue bv=search(bk);
-		    if (bv==null) { bv=new bookValue(); }
-		    switch (gameOverValue) {
-			    case Traxboard.DRAW:  bv.draw++; break;
-			    case Traxboard.WHITE: bv.white++; break;
-			    case Traxboard.BLACK: bv.black++; break;
-			    default:
-				  // This should never happen
-				  throw new RuntimeException("\n"+tb+"tb.isGameOver()="+tb.isGameOver());
-		    }
-		    theBook.put(bk,bv);
-	    }
-	  }
+                    bookKey bk = new bookKey(tb.getBorder(), tb.whoToMove());
+                    bookValue bv = search(bk);
+                    if (bv == null) {
+                        bv = new bookValue();
+                    }
+                    switch (gameOverValue) {
+                        case Traxboard.DRAW:
+                            bv.draw++;
+                            break;
+                        case Traxboard.WHITE:
+                            bv.white++;
+                            break;
+                        case Traxboard.BLACK:
+                            bv.black++;
+                            break;
+                        default:
+                            // This should never happen
+                            throw new RuntimeException("\n" + tb + "tb.isGameOver()=" + tb.isGameOver());
+                    }
+                    theBook.put(bk, bv);
+                }
+            }
+        } else {
+            while ((s = reader.readLine()) != null) {
+                tb = new Traxboard();
+                try {
+                    for (String move : s.split("\\s")) {
+                        tb.makeMove(move);
+                    }
+                    bookKey bk = new bookKey(tb.getBorder(), tb.whoDidLastMove());
+
+                    theBook.remove(bk);
+                    bookValue bv = new bookValue();
+                    bv.alwaysPlay = true;
+                    theBook.put(bk, bv);
+                } catch (IllegalMoveException e) {;
+                }
+            }
         }
-      else {
-	   while ( (s=reader.readLine()) != null) {
-		    tb=new Traxboard();
-		    try {
-			    for (String move : s.split("\\s")) { tb.makeMove(move); } 
-			    bookKey bk=new bookKey(tb.getBorder(),tb.whoDidLastMove());
-
-			    theBook.remove(bk);
-			    bookValue bv=new bookValue(); 
-			    bv.alwaysPlay=true;
-			    theBook.put(bk,bv);
-		    } catch (IllegalMoveException e) { ; }
-	  }
-       }
     }
 
-  public void loadBook() throws IOException {
-    loadBook("url");
-  } 
-
-  public void loadBook(String from) throws IOException {
-    String s;
-    BufferedReader reader;
-
-    if (from.equals("url")) {
-      reader=new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResource("games/book.bin").openStream()));
-    }
-    else {
-      reader=new BufferedReader(new FileReader(new File("games/book.bin")));
-    }
-    theBook=new HashMap<bookKey,bookValue>();
-    while ((s=reader.readLine())!=null) {
-      String[] elems=s.split("\\s");
-      if (elems.length==6) {
-        bookKey bk=new bookKey(elems[1],elems[0]);
-        bookValue bv=new bookValue(elems[2],elems[3],elems[4],elems[5]);
-        theBook.put(bk,bv);
-      }
-    }
-  }
-
-  private void saveBook() throws IOException {
-    BufferedWriter writer=new BufferedWriter(new FileWriter(new File("games/book.bin")));
-    for (Map.Entry<bookKey, bookValue> entry : theBook.entrySet()) {
-      if ((entry.getValue().alwaysPlay) || (entry.getValue().white+entry.getValue().black+entry.getValue().draw>2)) {
-        writer.write(entry.getKey()+" "+entry.getValue()+"\n");
-      }
-    }
-    writer.close();
-  }
-
-  public String toString() {
-    StringBuffer result=new StringBuffer();
-    for (Map.Entry<bookKey, bookValue> entry : theBook.entrySet()) {
-      result.append(entry.getKey()+" "+entry.getValue()+"\n");
-    }
-    return result.toString();
-  }
-    
-
-  public bookValue search(String border, int wtm) {
-          return this.search(new bookKey(border,wtm));
-  }
-
-  public bookValue search(bookKey bk) {
-          return theBook.get(bk);
-  }
-
-  public String info() {
-	  StringBuffer result=new StringBuffer();
-
-	  return result.toString();
-  }
-
-  public static void main(String[] args) {
-    Openingbook o=new Openingbook();
-    try {
-      o.setInputfile("games/alwaysplay.trx");
-      o.generateBook(true);
-      o.setInputfile("games/Trax.trx");
-      o.generateBook();
-      o.saveBook();
-    }
-    catch (Exception e) { System.err.println(e); }
-    try {
-      o.loadBook("disk");
-    }
-    catch (Exception e) { throw new RuntimeException(e); }
-    System.out.println("size of book: "+o.size());
-    System.out.println(o);
-  }
-
-  private class bookValue {
-    public int black, white, draw;
-    public boolean alwaysPlay;
-    public int score() {
-      if (alwaysPlay) return 1000;
-      return 0;
-    }
-    
-    public bookValue() { this(false); }
-  
-    public bookValue(boolean alwaysPlay, int black, int white, int draw) {
-      this.alwaysPlay=alwaysPlay; 
-      this.black=black;
-      this.white=0;
-      this.draw=0;
-    } 
-
-    public bookValue(String alwaysPlay, String black, String white, String draw) { 
-      this(alwaysPlay,Integer.parseInt(black),Integer.parseInt(white),Integer.parseInt(draw));
+    public void loadBook() throws IOException {
+        loadBook("url");
     }
 
-    public bookValue(String alwaysPlay, int black, int white, int draw) {
-      this.alwaysPlay=(alwaysPlay.equals("false")?false:true);
-      this.black=black;
-      this.white=white;
-      this.draw=draw;
-    }
-	
-    public bookValue(boolean alwaysPlay) { this(alwaysPlay,0,0,0); }
-     
-    public String toString() {
-      return ((alwaysPlay?"true":"false")+" "+black+" "+white+" "+draw);
-    }
-  }
+    public void loadBook(String from) throws IOException {
+        String s;
+        BufferedReader reader;
 
-  private class bookKey {
-    public int wtm;
-    public String border;
+        if (from.equals("url")) {
+            reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResource("games/book.bin").openStream()));
+        } else {
+            reader = new BufferedReader(new FileReader(new File("games/book.bin")));
+        }
+        theBook = new HashMap<bookKey, bookValue>();
+        while ((s = reader.readLine()) != null) {
+            String[] elems = s.split("\\s");
+            if (elems.length == 6) {
+                bookKey bk = new bookKey(elems[1], elems[0]);
+                bookValue bv = new bookValue(elems[2], elems[3], elems[4], elems[5]);
+                theBook.put(bk, bv);
+            }
+        }
+    }
+
+    private void saveBook() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("games/book.bin")));
+        for (Map.Entry<bookKey, bookValue> entry : theBook.entrySet()) {
+            if ((entry.getValue().alwaysPlay) || (entry.getValue().white + entry.getValue().black + entry.getValue().draw > 2)) {
+                writer.write(entry.getKey() + " " + entry.getValue() + "\n");
+            }
+        }
+        writer.close();
+    }
 
     public String toString() {
-      return wtm+" "+border;
+        StringBuffer result = new StringBuffer();
+        for (Map.Entry<bookKey, bookValue> entry : theBook.entrySet()) {
+            result.append(entry.getKey() + " " + entry.getValue() + "\n");
+        }
+        return result.toString();
     }
 
-    public bookKey(String border, int wtm) {
-      this.wtm=wtm;
-      this.border=border;
+    public bookValue search(String border, int wtm) {
+        return this.search(new bookKey(border, wtm));
     }
 
-    public bookKey(String border, String wtm) {
-      this(border,Integer.parseInt(wtm));
+    public bookValue search(bookKey bk) {
+        return theBook.get(bk);
     }
 
-    public bookKey(Traxboard tb) {
-      this(tb.getBorder(),tb.whoToMove());
+    public bookValue search(Traxboard tb) {
+	return this.search(tb.getBorder(), tb.whoToMove());
     }
 
-    public int hashCode() { return border.hashCode()+wtm; }
-   
-    public boolean equals(Object obj) {
-      if (this==obj) return true;
-      if ((obj == null) || (obj.getClass() != this.getClass())) return false;
-      bookKey oe=(bookKey)obj;
-      return (oe.wtm==this.wtm && oe.border.equals(this.border));
+    public String info() {
+        StringBuffer result = new StringBuffer();
+
+        return result.toString();
     }
-  }
+
+    public static void main(String[] args) {
+        Openingbook o = new Openingbook();
+        try {
+            o.setInputfile("games/alwaysplay.trx");
+            o.generateBook(true);
+            o.setInputfile("games/Trax.trx");
+            o.generateBook();
+            o.saveBook();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        try {
+            o.loadBook("disk");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("size of book: " + o.size());
+        System.out.println(o);
+    }
+
+    public class bookValue {
+
+        public int black, white, draw;
+        public boolean alwaysPlay;
+
+        public int score() {
+            if (alwaysPlay) { return Integer.MAX_VALUE; }
+	    return (black/(black+white+draw+1));
+	}
+
+        public bookValue() { this(false); }
+
+        public bookValue(boolean alwaysPlay, int black, int white, int draw) {
+            this.alwaysPlay = alwaysPlay;
+            this.black = black;
+            this.white = 0;
+            this.draw = 0;
+        }
+
+        public bookValue(String alwaysPlay, String black, String white, String draw) {
+            this(alwaysPlay, Integer.parseInt(black), Integer.parseInt(white), Integer.parseInt(draw));
+        }
+
+        public bookValue(String alwaysPlay, int black, int white, int draw) {
+            this.alwaysPlay = (alwaysPlay.equals("false") ? false : true);
+            this.black = black;
+            this.white = white;
+            this.draw = draw;
+        }
+
+        public bookValue(boolean alwaysPlay) {
+            this(alwaysPlay, 0, 0, 0);
+        }
+
+        public String toString() {
+            return ((alwaysPlay ? "true" : "false") + " " + black + " " + white + " " + draw);
+        }
+    }
+
+    private class bookKey {
+
+        public int wtm;
+        public String border;
+
+        public String toString() {
+            return wtm + " " + border;
+        }
+
+        public bookKey(String border, int wtm) {
+            this.wtm = wtm;
+            this.border = border;
+        }
+
+        public bookKey(String border, String wtm) {
+            this(border, Integer.parseInt(wtm));
+        }
+
+        public bookKey(Traxboard tb) {
+            this(tb.getBorder(), tb.whoToMove());
+        }
+
+        public int hashCode() {
+            return border.hashCode() + wtm;
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if ((obj == null) || (obj.getClass() != this.getClass())) {
+                return false;
+            }
+            bookKey oe = (bookKey) obj;
+            return (oe.wtm == this.wtm && oe.border.equals(this.border));
+        }
+    }
 }
